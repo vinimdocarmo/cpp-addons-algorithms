1f01b3696b2dac745dc41bc2c0eff27243cef53e
Vinícius do Carmo <vinicius.m.do.carmo@gmail.com>
2015-11-01 17:04:45 -0300

    Add test for selection sort.

--- test/sorting_algorithms.js
+++ test/sorting_algorithms.js
@@ -3,7 +3,7 @@ var algorithms = require('bindings')('algorithms'),
 
 describe('Sorting algorithms > ', function() {
    
-   it('insertion sort', function(done) {
+   it('insertion sort', function() {
       var a = [200, 45, 7];
 
       algorithms.insertionSort(a);
@@ -12,8 +12,17 @@ describe('Sorting algorithms > ', function() {
       a = [345, 355, 6, 0, 1, 12, 544, 123, 333, 4556, 67, 5, 1, 1, 1];
       algorithms.insertionSort(a);
       expect([0, 1, 1, 1, 1, 5, 6, 12, 67, 123, 333, 345, 355, 544, 4556]).to.deep.equal(a);
+   });
 
-      done();
+   it('selection sort', function () {
+  	var a = [200, 45, 7];
+
+	algorithms.selectionSort(a);
+	expect([7, 45, 200]).to.deep.equal(a);
+
+	a = [345, 355, 6, 0, 1, 12, 544, 123, 4556, 67, 5, 1, 1, 1];
+	algorithms.selectionSort(a);
+	expec([0, 1, 1, 1, 1, 5, 6, 12, 67, 123, 333, 345, 355, 544, 4556]).to.deep.equal(a);
    });
 
 });
